90b9639b4e04f2ab037d7060d73caa55db04b7ca
Helena <774866212@qq.com>
2025-07-24 13:06:03 -0400

    add service server and config server

---
+++ service-registry/src/main/java/com/mock/exchange/service_registry/ServiceRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.mock.exchange.service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// This class starts a Spring Boot application that acts as a Eureka Server
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
